b4259ed9833ea844a26ea6f3fab8b2e51b20b57b
laptop <antony_epis@hotmail.com>
2018-03-08 23:14:30 -0500

    fix code greet bot

--- BritanicoBot-src/Dialogs/MainDialog.cs
+++ BritanicoBot-src/Dialogs/MainDialog.cs
@@ -20,10 +20,11 @@ namespace SimpleEchoBot.Dialogs
         {
             Session.Codigo = null;
             Session.Result = false;
-           /* var message = context.MakeMessage();
-             message.Text = $"¡Hola, soy Merlí! el asistente virtual del BRITÁNICO. Permíteme ayudarte en los siguientes temas:";
-             await context.PostAsync(message);          
-             context.Wait(ResumeAfter);*/
+            /* var message = context.MakeMessage();
+              message.Text = $"¡Hola, soy Merlí! el asistente virtual del BRITÁNICO. Permíteme ayudarte en los siguientes temas:";
+              await context.PostAsync(message);          
+              context.Wait(ResumeAfter);*/
+            Session.Greet = false;
             context.Wait(this.MessageReceivedAsync);
         }
         public async Task ResumeAfter(IDialogContext context, IAwaitable<object> result)
